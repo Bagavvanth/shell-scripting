e8e01b24473a3c9a8f532df8e3f54dddcab10ed4
bagavvanth <bagavvanth8@gmail.com>
2024-12-30 22:22:55 +0530

    conversation added

---
+++ 04-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2 #no space before and after the equal to
+
+echo "$PERSON1:: hello $PERSON2"
+echo "$PERSON2:: Hello $PERSON1"
+echo "$PERSON1:: Have you had your dinner"
+echo "$PERSON2:: I had thanks for asking, How about you?" 
