0f3d2baa79a609ca32c48f8ca1130cf6790fd0a2
bagavvanth <bagavvanth8@gmail.com>
2024-12-30 21:54:42 +0530

    conversation added

--- 02-variable.sh
+++ 02-variable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo: "Ramesh:: hello Bagavanth"
-echo: "Bagavanth:: Hello Ramesh"
-echo: "Ramesh:: Have you had your dinner"
-echo: "Bagavanth:: I had thanks for asking, How about you"
+echo "Ramesh:: hello Bagavanth"
+echo "Bagavanth:: Hello Ramesh"
+echo "Ramesh:: Have you had your dinner"
+echo "Bagavanth:: I had thanks for asking, How about you?"
