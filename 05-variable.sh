2686414bfb615a404428f821c0784fbbf69e78af
bagavvanth <bagavvanth8@gmail.com>
2024-12-30 22:44:12 +0530

    conversation added

--- 05-variable.sh
+++ 05-variable.sh
@@ -5,3 +5,7 @@ echo "please enter your name::" #Text entered here will be added as value to var
 read -s USERNAME #Here USERNAME is the variable name
 
 echo "usename entered: $USERNAME"
+
+echo "please enter your password::"
+
+read -s PASSWORD #Here PASSWORD is the vaiable name
